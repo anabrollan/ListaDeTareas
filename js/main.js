148febb08814a81b1161aba4463352464c3a03be
anabrollan <anabrollan@gmail.com>
2024-05-22 20:03:31 -0400

    Nueva versión JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function agregarTarea() {
+/*function agregarTarea() {
     let nuevaTareaTexto = document.getElementById("nuevaTarea").value;
 
     if (nuevaTareaTexto === "") {
@@ -18,4 +18,42 @@ function agregarTarea() {
 
     document.getElementById("nuevaTarea").value = "";
 
+}*/
+let tareas = [];
+
+function agregarTarea() {
+  let nuevaTareaTexto = document.getElementById("nuevaTarea").value;
+
+  if (nuevaTareaTexto === "") {
+    alert("Por favor, ingresa una tarea");
+    return;
+  }
+
+  tareas.push(nuevaTareaTexto);
+  mostrarListaTareas();
+  document.getElementById("nuevaTarea").value = "";
+}
+
+function mostrarListaTareas() {
+  let listaTareas = document.getElementById("listaTareas");
+  listaTareas.innerHTML = "";
+
+  tareas.forEach(function(tarea, indice) {
+    let nuevaTarea = document.createElement("li");
+    nuevaTarea.textContent = tarea + " ";
+
+    let botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "Eliminar";
+    botonEliminar.onclick = function() {
+      eliminarTarea(indice);
+    };
+
+    nuevaTarea.appendChild(botonEliminar);
+    listaTareas.appendChild(nuevaTarea);
+  });
+}
+
+function eliminarTarea(indice) {
+  tareas.splice(indice, 1); 
+  mostrarListaTareas();
 }
